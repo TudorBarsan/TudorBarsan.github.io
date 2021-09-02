dd5379670796815aeddd2777acce061e01dba813
Tudor Barsan <65683781+TudorBarsan@users.noreply.github.com>
2021-09-02 16:55:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+let root = document.documentElement;
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+    root.style.setProperty('--clr-light');
+    
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+      document.body.classList.remove('nav-open');
+  })
+})
+
+let checkbox = document.querySelector("input[name=theme]");
+checkbox.addEventListener("change", function(){
+  if(this.checked){
+    document.documentElement.setAttribute("data-theme", "dark");
+    document.getElementById('logo').src="Images/Img placeholder.png"
+  } else{
+    document.documentElement.setAttribute("data-theme", "light");
+    document.getElementById('logo').src="Images/TBlogo.png"
+  }
+})
